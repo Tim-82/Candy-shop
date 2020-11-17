c32fd2631f99d0d481917e01652ba69c9ae90029
Tim-82 <barboza-82@mail.ru>
2020-11-17 23:45:46 +0300

    finished first product item

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(function(){
         $(this).addClass('tab--active');
         $($(this).attr('href')).addClass('tabs-content--active');
     });
-    // $('.product-item__favorite').on('click', function() {
-    //     $('.product-item__favorite').toggleClass('product-item__favorite--active')
-    // })
+    $('.product-item__favorite').on('click', function() {
+        $('.product-item__favorite').toggleClass('product-item__favorite--active')
+    })
 });
