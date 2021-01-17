1df65d2c1eaf74658652e5f808a4624af7f94553
Tim-82 <barboza-82@mail.ru>
2021-01-18 01:06:17 +0300

    added slider for brand-section

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,21 @@
  
-const glide1 = document.getElementById("glide1");
-
+const slider1 = document.getElementById("glide1");
 
 if (glide1)
-  new Glide(glide1, {
+  new Glide(slider1, {
     type: "carousel",
     startAt: 0,
-    autoplay: 3000,
-    hoverpause: true,
-    perView: 1,
+    perView: 5,
+    hoverpause: false,
+    autoplay: 2000,
     animationDuration: 800,
-    animationTimingFunc: "linear",
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      1200: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+    },
   }).mount();
