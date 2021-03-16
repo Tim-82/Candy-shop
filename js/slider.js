8e579fe2d4beeb809ecb3df0c0a4c6bfa1f66612
Tim-82 <barboza-82@mail.ru>
2021-03-16 19:30:13 +0300

    added Latest products-section and slider

---
+++ js/slider.js
@@ -0,0 +1,35 @@
+/*
+=============
+Glide Js Carousel
+=============
+ */
+
+const slider2 = document.getElementById("glide_2");
+
+/*
+=============
+Latest Products
+=============
+ */
+
+if (slider2) {
+	new Glide("#glide_2", {
+		type: "carousel",
+		startAt: 0,
+		perView: 4,
+		rewin: false,
+		animationDuration: 800,
+		animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+		breakpoints: {
+			1200: {
+				perView: 3,
+			},
+			768: {
+				perView: 2,
+			},
+		},
+	}).mount();
+}
+
+
+AOS.init();
